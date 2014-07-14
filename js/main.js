d98dac33bfe682ce23db02a2ff9515d3145e4ec9
ParkMinkyu <niee@naver.com>
2014-07-14 15:38:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -85,8 +85,7 @@ function getComment(){
 				message = message.replace(new RegExp('\\<','g'),'&lt;');
 				message = message.replace(new RegExp('\\>','g'),'&gt;');
 				var $li = $('<li>',{
-					html : "<p style='white-space: initial;'>"+message +"<br>( "+ date.getFullYear() + "년 " + (date.getMonth()+1)+"월 "+ date.getDate()+"일"+" )</p>",
-					css : {"font-size" : "15px"} 
+					html : "<p style='white-space: initial;font-size:14px;'>"+message +"<br><span style='font-weight:bold;'>( "+ date.getFullYear() + "년 " + (date.getMonth()+1)+"월 "+ date.getDate()+"일"+" )</span></p>"
 				});
 				$('ul').append($li);
 			}

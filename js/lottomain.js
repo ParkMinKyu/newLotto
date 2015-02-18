df66e4af8afa469b24e46c475246fa1c52a1957e
ParkMinkyu <niee@naver.com>
2015-02-18 17:30:22 +0900

    Update lottomain.js

--- js/lottomain.js
+++ js/lottomain.js
@@ -104,11 +104,13 @@ function showPop(type){
 		if(delBall.length > 0){
 			var $objs = $('#delballList').find('div');
  			for(var i = 0 ; i < $objs.length ; i++){
- 				CallAndroid.Alert(delBall.indexOf((i+1)));
- 				if(delBall.indexOf((i+1)) != -1)
+ 				if(delBall.indexOf((i+1)) != -1){
+ 					CallAndroid.Alert($($objs[i]).attr('class'));
  					$($objs[i]).removeClass('noMatch');
- 				else 
+ 				}
+ 				else {
  					$($objs[i]).addClass('noMatch');
+ 				}
  			}
 		}
 		$('#delHref').click();

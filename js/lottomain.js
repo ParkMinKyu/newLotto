bb0d9fcc943a00d5f51285c000d86f951c48907d
ParkMinkyu <niee@naver.com>
2015-02-18 17:22:04 +0900

    Update lottomain.js

--- js/lottomain.js
+++ js/lottomain.js
@@ -104,7 +104,7 @@ function showPop(type){
 		if(delBall.length > 0){
 			var $objs = $('#delballList').find('div');
  			for(var i = 0 ; i < $objs.length ; i++){
- 				if(delBall.[i] != null)
+ 				if(delBall[i] != null)
  					$($objs[i]).removeClass('noMatch');
  			}
 		}

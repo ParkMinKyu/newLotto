d0a22494e7e535789374df73758f2f1f53be6b2a
ParkMinkyu <niee@naver.com>
2015-02-18 17:28:48 +0900

    Update lottomain.js

--- js/lottomain.js
+++ js/lottomain.js
@@ -104,6 +104,7 @@ function showPop(type){
 		if(delBall.length > 0){
 			var $objs = $('#delballList').find('div');
  			for(var i = 0 ; i < $objs.length ; i++){
+ 				CallAndroid.Alert(delBall.indexOf((i+1)));
  				if(delBall.indexOf((i+1)) != -1)
  					$($objs[i]).removeClass('noMatch');
  				else 

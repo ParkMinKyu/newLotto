08d92b781fa7bbea2b981c57ff54f05eb4f4de0f
ParkMinkyu <niee@naver.com>
2017-01-05 15:15:20 +0900

    Update kakaomain2017.js

--- js/kakaomain2017.js
+++ js/kakaomain2017.js
@@ -12,7 +12,12 @@ $(function(){
 	});
 	
 	$('#choiceSave').on('click',function(){
-		createChoiceBall();
+		var count = parseInt($('select[name=one]').val())+parseInt($('select[name=two]').val())+parseInt($('select[name=three]').val())+parseInt($('select[name=four]').val())+parseInt($('select[name=five]').val());
+		if(count < 6){
+			CallAndroid.Alert('각 범위 총 합을 6으로 맞춰 주세요.');
+		}else{
+			createChoiceBall();
+		}
 	});
 	
 	$('#randomSave').on('touchstart',function(e){
